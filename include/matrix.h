8cc3a75c17382de01cc75eaa7125ea63106e3c65
Vladimir Efremov <vefremov@gmail.com>
2020-06-06 15:31:13 +0200

    Passing references instead of copying arrays

--- include/matrix.h
+++ include/matrix.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <map>
 #include <array>
+#include <iostream>
 
 namespace OTUS
 {
@@ -13,27 +14,28 @@ class Matrix
     template<size_t DIM>
     struct helper_index
     {
-        helper_index(Matrix& m, std::array<size_t, DIM> ind): m_m{m}, m_ind(ind) {}
+        helper_index(Matrix& m, std::array<size_t, N_DIM>& ind): m_m{m}, m_ind(ind) {}
         helper_index<DIM+1> operator[](size_t j)
         {
-            std::array<size_t, DIM+1> ind_next;
-            for (size_t i = 0; i < DIM; i++)
-            {
-                ind_next[i] = m_ind[i];
-            }
-            ind_next[DIM] = j;
-            return helper_index<DIM+1>(m_m, ind_next);
+            // std::array<size_t, DIM+1> ind_next;
+            // for (size_t i = 0; i < DIM; i++)
+            // {
+            //     ind_next[i] = m_ind[i];
+            // }
+            m_ind[DIM] = j;
+            return helper_index<DIM+1>(m_m, m_ind);
         }
         Matrix& m_m;
-        std::array<size_t, DIM> m_ind;
+        std::array<size_t, N_DIM>& m_ind;
     };
 
     template<>
     struct helper_index<N_DIM>
     {
-        helper_index(Matrix& m, std::array<size_t, N_DIM> ind): m_m{m}, m_ind{ind} {}
+        helper_index(Matrix& m, std::array<size_t, N_DIM>& ind): m_m{m}, m_ind{ind} {}
         helper_index<N_DIM>& operator= (T val) 
         {
+            std::cout << "ijv " << m_ind[0] << " " << m_ind[1] << " " << val << std::endl;
             m_m.put(val, m_ind);
             return *this;
         }
@@ -42,7 +44,7 @@ class Matrix
             return m_m.get(m_ind);
         }
         Matrix& m_m;
-        std::array<size_t, N_DIM> m_ind;
+        std::array<size_t, N_DIM>& m_ind;
     };
 
     class m_iterator
@@ -101,9 +103,10 @@ class Matrix
 
     helper_index<1> operator[](size_t i)
     {   
-        std::array<size_t, 1> ind;
-        ind[0] = i;
-        return helper_index<1>(*this, ind);
+        // std::array<size_t, N_DIM> ind;
+        // ind[0] = i;
+        m_cur_index[0] = 1;
+        return helper_index<1>(*this, m_cur_index);
     }
     size_t size()
     {
@@ -122,6 +125,7 @@ class Matrix
     std::map<std::array<size_t, N_DIM>, T> m_map;
     
     T m_default = DEF;
+    std::array<size_t, N_DIM> m_cur_index;
 };
 
 }
